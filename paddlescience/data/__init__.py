3a1c90b28150492644a5e53ff8263edad974fec7
zhiminzhang0830 <452516515@qq.com>
2023-03-22 06:19:32 +0000

    fix code style according to pep8

--- paddlescience/data/__init__.py
+++ paddlescience/data/__init__.py
@@ -11,16 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import types
+
+import paddle
 import paddle.distributed as dist
-from paddle.io import BatchSampler
-from paddle.io import DataLoader
-from paddle.io import DistributedBatchSampler
+
+from paddlescience.data import trphysx_dataset
 
 from .data_process import load_data
 from .data_process import save_data
-from .trphysx_dataset import CylinderDataset
-from .trphysx_dataset import LorenzDataset
-from .trphysx_dataset import RosslerDataset
 
 
 def build_dataloader(
@@ -31,23 +31,32 @@ def build_dataloader(
     dataset_name - name of the dataset
     batch_size - batch size,
     shuffle - is shuffle data
-    drop_last -"""
+    drop_last - whether drop the last incomplete batch dataset size
+            is not divisible by the batch size.
+    """
+    dataset_args = types.MappingProxyType(dataset_args)
 
-    assert dataset_name in ["LorenzDataset", "CylinderDataset", "RosslerDataset"]
-    dataset = eval(dataset_name)(**dataset_args)
+    if dataset_name == "LorenzDataset":
+        dataset = trphysx_dataset.LorenzDataset(**dataset_args)
+    elif dataset_name == "CylinderDataset":
+        dataset = trphysx_dataset.CylinderDataset(**dataset_args)
+    elif dataset_name == "RosslerDataset":
+        dataset = trphysx_dataset.RosslerDataset(**dataset_args)
+    else:
+        raise NotImplementedError(f"{dataset_name} is not implemented.")
 
     if dist.get_world_size() > 1:
         # Distribute data to multiple cards
-        batch_sampler = DistributedBatchSampler(
+        batch_sampler = paddle.io.DistributedBatchSampler(
             dataset=dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
         )
     else:
         # Distribute data to single card
-        batch_sampler = BatchSampler(
+        batch_sampler = paddle.io.BatchSampler(
             dataset=dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
         )
 
-    data_loader = DataLoader(
+    data_loader = paddle.io.DataLoader(
         dataset=dataset, batch_sampler=batch_sampler, num_workers=num_workers
     )
 
