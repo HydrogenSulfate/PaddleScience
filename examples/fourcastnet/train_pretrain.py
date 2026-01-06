8729da7762ebd5d97628077f86850b985a447fa4
HydrogenSulfate <490868991@qq.com>
2026-01-06 10:56:19 +0800

    update

--- examples/fourcastnet/train_pretrain.py
+++ examples/fourcastnet/train_pretrain.py
@@ -172,7 +172,7 @@ def train(cfg: DictConfig):
         ITERS_PER_EPOCH,
         eval_during_train=False,
         seed=cfg.seed,
-        update_freq=100,
+        update_freq=cfg.TRAIN.update_freq,
         # validator=validator,
         compute_metric_by_batch=cfg.EVAL.compute_metric_by_batch,
         eval_with_no_grad=cfg.EVAL.eval_with_no_grad,
